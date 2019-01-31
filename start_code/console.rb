ddeca2578e8de4b2002d89ec1c12222055201799
Alexkcshing <alexkcshing@gmail.com>
2019-01-31 14:39:16 +0000

    Create constructors for classes

---
+++ start_code/console.rb
@@ -0,0 +1,25 @@
+require_relative("models/casting")
+require_relative("models/movie")
+require_relative("models/star")
+
+require('pry')
+
+# Casting.delete_all()
+# Movie.delete_all()
+# Star.delete_all()
+
+movie1 = Movie.new({ 'title' => 'Cloud Atlas', 'genre' => 'Sci-Fi'} )
+movie2 = Movie.new({ 'title' => 'Forrest Gump', 'genre'=>'Drama' } )
+
+star1 = Star.new({'first_name' => 'Tom', 'last_name' => 'Hanks'})
+star2 = Star.new({'first_name' => 'Hale', 'last_name' => 'Berry'})
+star3 = Star.new({'first_name' => 'Robin', 'last_name' => 'Wright'})
+
+casting1 = Casting.new({'movie_id' => movie1.id, 'star_id'=> star1.id, 'fee'=> 5_000_000})
+casting2 = Casting.new({'movie_id' => movie1.id, 'star_id'=> star2.id, 'fee'=> 5_000_000})
+casting3 = Casting.new({'movie_id' => movie2.id, 'star_id'=> star1.id, 'fee'=> 4_000_000})
+casting1 = Casting.new({'movie_id' => movie2.id, 'star_id'=> star3.id, 'fee'=> 4_000_000})
+
+
+binding.pry
+nil
